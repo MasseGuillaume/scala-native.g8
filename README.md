f1abccc76964ed457912055009a6d2a7bb076b85
Guillaume Massé <masgui@gmail.com>
2017-04-28 12:57:57 -0400

    g8Test setup

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-A [Giter8][g8] template for Scala-Native
+[![Join the chat at https://gitter.im/scala-native/scala-native](https://badges.gitter.im/scala-native/scala-native.svg)](https://gitter.im/scala-native/scala-native)
+[![Build Status](https://travis-ci.org/scala-native/scala-native.g8.svg?branch=master)](https://travis-ci.org/scala-native/scala-native.g8)
+
+A [Giter8](http://www.foundweekends.org/giter8/) template for Scala-Native
+
+Make sure to install the required system libraries: http://www.scala-native.org/en/latest/user/setup.html
 
 ```
-sbt new MasseGuillaume/scala-native.g8
+sbt new scala-native/scala-native.g8
 ```
-
-[g8]: http://www.foundweekends.org/giter8/
--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,5 +1,31 @@
+import scalanative.native._
+
+object posixh {
+  type time_t      = CLong
+  type suseconds_t = CLong
+  type timeval     = CStruct2[time_t, suseconds_t]
+  type timezone    = CStruct0
+
+  implicit class timevalOps(val ptr: Ptr[timeval]) extends AnyVal {
+    def sec: time_t       = !(ptr._1)
+    def usec: suseconds_t = !(ptr._2)
+  }
+}
+
+@extern
+object posix {
+  import posixh._
+  def gettimeofday(tv: Ptr[timeval], tz: Ptr[timezone]): Unit = extern
+}
+
+import posix._, posixh._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val tv = stackalloc[timeval]
+    gettimeofday(tv, null)
+
+    println("Hello, world!")
+    println("Time: " + tv.sec)
   } 
 }
